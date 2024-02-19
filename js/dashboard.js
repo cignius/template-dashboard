4f9c157e2e4137c004512999ad97a8ee108f568f
Irwing Soto <irwings@holagod.com>
2024-02-19 03:04:20 -0600

    Derechos de autor

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,5 @@
+/*****************************/
+/****** Elaborado por ********/
+/*Irwing Eduardo Soto Ramirez*/
+/*****************************/
 !function(){"use strict";var i=document.getElementById("graphkpi");new Chart(i,{type:"line",data:{labels:["kpi1","kpi2","kpi3","kpi4","kpi5","kpi6","kpi7"],datasets:[{data:[10,18,10,12,10,20,],lineTension:0,backgroundColor:"transparent",borderColor:"#b8c0c6",borderWidth:2,pointRadius:0,pointBackgroundColor:"transparent"}]},options:{scales:{yAxes:[{ticks:{display:!1,beginAtZero:!0},gridLines:{drawOnChartArea:!1,lineWidth:3,drawTicks:!1}}],xAxes:[{ticks:{display:!1},gridLines:{drawOnChartArea:!1,lineWidth:3,drawTicks:!1}}]},legend:{display:!1}}})}();
